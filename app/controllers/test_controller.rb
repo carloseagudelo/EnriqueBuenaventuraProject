a3bfa3689dbe6124bcfc1b1521e10fdc1a1e53c6
Carlos Agudelo <carloskikea@hotmail.com>
2015-06-18 12:14:00 -0500

    Se modificaron en la vista principal acceso a logeo y registro a usuario

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -1,4 +1,6 @@
 class TestController < ApplicationController
+
   def test
   end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'test/test'
 
-  get 'index/index'
+  resource :test
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
