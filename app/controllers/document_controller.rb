26f098474e21e3c88ba5af71974d3810c57dec1b
Carlos Agudelo <carloskikea@hotmail.com>
2015-06-24 23:39:11 -0500

    Se agregan diseños a las vistas y el controlador para la primera aprte del 'documentos'

---
+++ app/controllers/document_controller.rb
@@ -0,0 +1,6 @@
+class DocumentController < ApplicationController
+
+	def index
+	end
+	
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   get 'welcome/index'
 
+  get 'document/index'
+
   root :to => "welcome#index"
 
   # The priority is based upon order of creation: first created -> highest priority.
