7995688375c68d4e4ca9779413800b8f37ef9d39
Carlos <carloskikea@gmail.com>
2016-01-06 23:17:14 -0500

    ultimos commits

---
+++ public/uploads/tmp/1447109137-2898-8440/thumb_EnriqueBuenaventura.sql
@@ -0,0 +1,59 @@
+--TIPO DE DOCUMENTO
+INSERT INTO specialities (id, name) VALUES ('1','Articulo');
+INSERT INTO specialities (id, name) VALUES ('2','Carta');
+INSERT INTO specialities (id, name) VALUES ('3','Conferencia');
+INSERT INTO specialities (id, name) VALUES ('4','Ensayo');
+INSERT INTO specialities (id, name) VALUES ('5','Folleto');
+INSERT INTO specialities (id, name) VALUES ('6','Manifiesto');
+INSERT INTO specialities (id, name) VALUES ('7','Programa Académico');
+INSERT INTO specialities (id, name) VALUES ('8','Proyecto de Investigación');
+
+--SUBTEMA
+INSERT INTO subtopics (id, name) VALUES ('1','Adaptaciones y Versiones');
+INSERT INTO subtopics (id, name) VALUES ('2','Dramaturgia');
+INSERT INTO subtopics (id, name) VALUES ('3','Estudios de Antropoligía');
+INSERT INTO subtopics (id, name) VALUES ('4','Estudios de Lingüística');
+INSERT INTO subtopics (id, name) VALUES ('5','Estudios de Historia del Teatro');
+INSERT INTO subtopics (id, name) VALUES ('6','Estudios Sobre Bertolt Brecht');
+INSERT INTO subtopics (id, name) VALUES ('7','Método de Creación Coleciva');
+INSERT INTO subtopics (id, name) VALUES ('8','Sobre la Enseñanza Teatral');
+INSERT INTO subtopics (id, name) VALUES ('9','Teaatro y Cultura');
+INSERT INTO subtopics (id, name) VALUES ('10','Temas Diversos');
+
+--PROSA BASE
+INSERT INTO proses (id, name) VALUES ('1','Análisis de Cuento');
+INSERT INTO proses (id, name) VALUES ('2','Argumentativa');
+INSERT INTO proses (id, name) VALUES ('3','Didáctica');
+INSERT INTO proses (id, name) VALUES ('4','Ensayo');
+INSERT INTO proses (id, name) VALUES ('5','Expositiva');
+INSERT INTO proses (id, name) VALUES ('6','Hipótesis');
+INSERT INTO proses (id, name) VALUES ('7','Informativa');
+INSERT INTO proses (id, name) VALUES ('8','Narrativa');
+INSERT INTO proses (id, name) VALUES ('9','Poética');
+
+--AUTORES TRABAJADOS
+INSERT INTO authors (id, name) VALUES ('1','Gabriel Garcia Marquez');
+INSERT INTO authors (id, name) VALUES ('2','Oscar Wilde');
+INSERT INTO authors (id, name) VALUES ('3','Dan Drawn');
+INSERT INTO authors (id, name) VALUES ('4','Paulo Coelho');
+INSERT INTO authors (id, name) VALUES ('5','Miguel de Cervantes Saavedra');
+INSERT INTO authors (id, name) VALUES ('6','Homero');
+INSERT INTO authors (id, name) VALUES ('7','Alejandro Dumas');
+INSERT INTO authors (id, name) VALUES ('8','Isabelle Allende');
+INSERT INTO authors (id, name) VALUES ('9','Carlos Ruiz Zafón');
+INSERT INTO authors (id, name) VALUES ('10','Umberto Eco');
+
+
+--LIBROS TRABAJADOS
+INSERT INTO books (id, name) VALUES ('1','Cien Años de Soledad');
+INSERT INTO books (id, name) VALUES ('2','La Odisea');
+INSERT INTO books (id, name) VALUES ('3','El coronel no Tiene Quien le Escriba');
+INSERT INTO books (id, name) VALUES ('4','La iliada');
+INSERT INTO books (id, name) VALUES ('5','El Ingenioso Hidalg Don Quijote');
+INSERT INTO books (id, name) VALUES ('6','El Alquimista');
+INSERT INTO books (id, name) VALUES ('7','El Perfume');
+INSERT INTO books (id, name) VALUES ('8','El Amor en los Tiempos de Colera');
+INSERT INTO books (id, name) VALUES ('9','Angeles y Demonios');
+INSERT INTO books (id, name) VALUES ('10','La Divina Comedia');
+
+
