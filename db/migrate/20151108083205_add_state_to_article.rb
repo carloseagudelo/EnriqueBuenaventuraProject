c8dfdf407b90fa9104e8b7b1eb679b43c835ca09
Carlos <carloskikea@gmail.com>
2016-01-11 22:21:28 -0500

    Se organizan detalles finales en las vistas y funcionalidades adicionales

--- db/migrate/20151108083205_add_state_to_article.rb
+++ db/migrate/20151108083205_add_state_to_article.rb
@@ -1,6 +1,6 @@
 class AddStateToArticle < ActiveRecord::Migration
   def change
-    add_column :articles, :stata, :boolean, :default => false
+    add_column :articles, :stata, :boolean, :default => true
     add_column :articles, :statepublic, :boolean, :default => false
   end
 end
