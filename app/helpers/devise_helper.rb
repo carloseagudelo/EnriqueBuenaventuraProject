408c5eb9636d6ea3aace261bc40e6823401567a9
Carlos <carloskikea@gmail.com>
2016-06-11 18:39:38 -0500

    Se mejora el codigo

---
+++ app/helpers/devise_helper.rb
@@ -0,0 +1,15 @@
+module DeviseHelper
+
+  def devise_error_messages!
+    flash_alerts = []
+    error_key = 'errors.messages.not_saved'
+
+    if !flash.empty?
+      flash_alerts.push(flash[:error]) if flash[:error]
+      flash_alerts.push(flash[:alert]) if flash[:alert]
+      flash_alerts.push(flash[:notice]) if flash[:notice]
+      error_key = 'devise.failure.invalid'
+    end
+  end
+
+end
